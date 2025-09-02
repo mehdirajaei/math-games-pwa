781303411b1a383ee54aa5ddc4f80465040e58e7
mehdirajaei <mr.salmasi@gmail.com>
2025-09-03 00:10:47 +0330

    Delete math-games-pwa-full directory

--- math-games-pwa-full/src/registerServiceWorker.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(reg => {
-      console.log('Service worker registered.', reg)
-    }).catch(err => console.log('SW registration failed:', err))
-  })
-}
--- math-games-pwa-full/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
-import pwaConfig from './vite-plugin-pwa.config.js'
-
-export default defineConfig({
-  plugins: [react(), VitePWA(pwaConfig)],
-})
